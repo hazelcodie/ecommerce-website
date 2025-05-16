0c902c312e22b9e2ab95ad938671c9b28057c824
hazelcodie <hazelmontiaora@gmail.com>
2025-05-16 08:25:47 +0800

    feat: add new UI components and utilities

--- app/add-product/page.tsx
+++ app/add-product/page.tsx
@@ -1,7 +1,65 @@
-export default function AddProductPage(){
-    return(
-        <div>
-            <h1 className="text-lg">Add Product</h1>
-        </div>
-    )
+import * as React from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+export default function AddProductPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Add new Product</CardTitle>
+        <CardDescription>Add your new product in one click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of the product" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Description</Label>
+              <Input id="name" placeholder="Description of the product" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="category">Category</Label>
+              <Select>
+                <SelectTrigger id="category">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="next">Clothing</SelectItem>
+                  <SelectItem value="sveltekit">Footwear</SelectItem>
+                  <SelectItem value="astro">Accessories</SelectItem>
+                  <SelectItem value="nuxt">Toys</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Add</Button>
+      </CardFooter>
+    </Card>
+    </div>
+  );
 }
